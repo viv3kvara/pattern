6bc5f7df5b785f96f536ad43eda1ffca7a9de69c
VARA VIVEK UMESHBHAI <vivekvara2004@gmail.com>
2025-08-21 16:49:29 +0530

    first commit

---
+++ HalfPyramid.java
@@ -0,0 +1,13 @@
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int n=5;
+        // int num = 15;
+        for(int i=1 ; i<=n; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
