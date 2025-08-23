301e6bb67ef947e9613e43d5d390fb9d3cf69d35
VARA VIVEK UMESHBHAI <vivekvara2004@gmail.com>
2025-08-23 13:52:05 +0530

    Third Commit

---
+++ Butterfly.java
@@ -0,0 +1,31 @@
+public class Butterfly {
+    public static void main(String[] args) {
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(4-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=4; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(4-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
---
+++ HollowTraingleInverted.java
@@ -0,0 +1,15 @@
+class HollowTraingleInverted {
+    public static void main(String[] args) {
+        for(int i=5; i>=1; i--){
+            
+            for(int j=1; j<=i; j++){
+                  if (i == 5 || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
